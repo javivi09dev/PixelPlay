999720d7e852a06e71ede9380af5053594eb4d19
Javier <javierotoalonso@gmail.com>
2025-08-08 20:28:05 +0200

    init

---
+++ src/main/java/me/javivi/pp/Pixelplay.java
@@ -0,0 +1,10 @@
+package me.javivi.pp;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Pixelplay implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/me/javivi/pp/client/PixelplayClient.java
@@ -0,0 +1,10 @@
+package me.javivi.pp.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PixelplayClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
